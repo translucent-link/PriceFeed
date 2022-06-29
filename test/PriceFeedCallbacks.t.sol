31019f27fd3ffb5afd802b80c551bba0c23e5554
Ijonas Kisselbach <ijonas@ijonas.com>
2022-06-29 15:27:10 +0100

    removed unused code

--- test/PriceFeedCallbacks.t.sol
+++ test/PriceFeedCallbacks.t.sol
@@ -47,20 +47,4 @@ contract PriceFeedTest is Test {
         assertEq(priceFeed.lastUpdatedTimestamp(), 1680000);
         assertEq(priceFeed.updatesReceived(), 0);
     }
-
-    function bytes32ToString(bytes32 _bytes32)
-        public
-        pure
-        returns (string memory)
-    {
-        uint8 i = 0;
-        while (i < 32 && _bytes32[i] != 0) {
-            i++;
-        }
-        bytes memory bytesArray = new bytes(i);
-        for (i = 0; i < 32 && _bytes32[i] != 0; i++) {
-            bytesArray[i] = _bytes32[i];
-        }
-        return string(bytesArray);
-    }
 }
