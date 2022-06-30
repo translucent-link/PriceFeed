33c50b91aa3a39237d16aae472592f55c82e8cae
Ijonas Kisselbach <ijonas@ijonas.com>
2022-06-30 16:52:55 +0100

    fleshed out Rinkeby deploy script

--- script/Contract.s.sol
+++ script/Contract.s.sol
@@ -2,11 +2,24 @@
 pragma solidity 0.8.7;
 
 import "forge-std/Script.sol";
+import "../src/PriceFeed.sol";
 
 contract ContractScript is Script {
-    function setUp() public {}
-
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+
+        uint256 minimumOracles = 1;
+        uint256 maximumOracles = 2;
+        uint256 payment = 5 * 10**16;
+        address linkTokenAddress = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
+
+        PriceFeed feed = new PriceFeed(
+            minimumOracles,
+            maximumOracles,
+            payment,
+            linkTokenAddress
+        );
+
+        vm.stopBroadcast();
     }
 }
